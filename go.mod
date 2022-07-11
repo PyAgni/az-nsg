df97fffb62fe3b1932143cd694c2dbb74da5feb8
Agni <bhattacharyya.agni@gmail.com>
2022-07-11 23:24:38 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module az-nsg
+
+go 1.17
+
+require github.com/Azure/go-autorest/autorest/azure/auth v0.5.11
+
+require (
+	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go v58.0.0+incompatible
+	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/Azure/go-autorest/autorest v0.11.24 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.18 // indirect
+	github.com/Azure/go-autorest/autorest/azure/cli v0.4.5 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/logger v0.2.1 // indirect
+	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/dimchansky/utfbom v1.1.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-11-01/network"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"log"
+)
+
+func main() {
+	fmt.Println("abcd")
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		log.Printf("Error while creating an new authentication, %v ", err)
+
+	}
+	//fmt.Println(authorizer)
+	var subscriptionId string = ""
+
+	nsgClient := network.NewSecurityGroupsClient(subscriptionId)
+	nsgClient.Authorizer = authorizer
+
+	group, error := nsgClient.Get(context.Background(), "demorg",
+		"staging-deployer-nsg", "")
+	if error != nil {
+		log.Printf("Abce %v", error)
+	}
+	fmt.Println(*group.ID)
+
+	rulesClient := network.NewSecurityRulesClient(subscriptionId)
+	rulesClient.Authorizer = authorizer
+
+	rule, _ := rulesClient.Get(context.Background(), "demorg",
+		"staging-deployer-nsg", "SSH")
+
+	var IPs []string = []string{"abvc"}
+	fmt.Println(IPs)
+
+	fmt.Println(*rule.SourceAddressPrefixes)
+	//fmt.Println(rule)
+	//rgClient := resources.NewGroupsClient(subscriptionId)
+	//rgClient.Authorizer = authorizer
+	//ctx := context.Background()
+	//for list, err := rgClient.ListComplete(ctx, "", nil); list.NotDone(); err = list.NextWithContext(ctx) {
+	//	if err != nil {
+	//		log.Fatalf("got error: %s", err)
+	//	}
+	//	rgName := *list.Value().Name
+	//	fmt.Println(rgName)
+	//}
+}
