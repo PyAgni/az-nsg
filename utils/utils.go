012823e36e89f8fed87c4fab7b4046c1739ba7d0
Agni <bhattacharyya.agni@gmail.com>
2022-07-12 23:45:54 +0530

    Added GetPublicIP func in utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,3 +1,47 @@
 package utils
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
 //add public ip logic
+
+// IP Struct for parsing response of public IP from https://api.ipify.org/?format=json
+type IP struct {
+	ip string
+}
+
+const IpApiUrl = "https://api.ipify.org/?format=json"
+
+func GetPublicIP() IP {
+	res, err := http.Get(IpApiUrl)
+	if err != nil {
+		fmt.Printf("Couldn't get your public IP. Error: %s\n", err)
+		return IP{}
+	}
+	//Close the response body
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Printf("Couldn't get your public IP. Error: %s\n", err)
+		}
+	}(res.Body)
+
+	//Read response body
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("Couldn't read IP from response. Error: %s\n", err)
+		return IP{}
+	}
+	var ip IP
+	err = json.Unmarshal(body, &ip)
+	if err != nil {
+		return IP{}
+	}
+
+	return ip
+}
